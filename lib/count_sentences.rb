71988ff1537c558402440bff320bb37b822db401
rahimedu <edwin.duche82@gmail.com>
2022-08-20 15:27:33 -0400

    commit tree 84b197b39f8dcab2014a7ed96fc985f17e5aafb1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    list = self.scan(/[A-Za-z ,]*[?!.]*/)
+    puts list
+    list.length - 1
   end
 end
